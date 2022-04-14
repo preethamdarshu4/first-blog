50c117895d65f689da9889953f5d63d67935e76e
darshu <preethamdarshu4411@gmail.com>
2022-04-14 11:32:29 +0530

    Modified templates to dynamically display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
+from turtle import pos
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone as tz
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=tz.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
